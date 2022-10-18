9366ff0e909722df188d6c0235e1d8d727518d82
Mujeeb Subhani <mujeeb.subhani78@gmail.com>
2022-10-18 13:55:18 +0500

    Edited

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "<h1>Project Started</h1>"
+    return "<i>Project Started</i>"
 
 
 if __name__=="__main__":
     app.run()
+
+# CI CD Pipeline =>
